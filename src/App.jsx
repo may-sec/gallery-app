76554d3be4c902417af6a6ddc5031154e06a6ae8
may-sec <165646909+may-sec@users.noreply.github.com>
2025-12-19 23:56:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,17 @@ import axios from 'axios'
 import Card from './components/Card';
 
 const App = () => {
-
   const [userData, setUserData] = useState([]);
-
   const [index, setIndex] = useState(1)
-
+  
   const getData = async () => {
-    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=10`)
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${index+1}&limit=12`)
     setUserData(response.data)
   }
 
   useEffect(function () {
     getData()
   }, [index])
-
 
   let printUserData = <h3 className='text-gray-300 text-xs absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold'>Loading...</h3>
 
@@ -31,6 +28,7 @@ const App = () => {
 
   return (
     <div className='bg-black overflow-auto h-screen p-4 text-white'>
+    <h1 className='text-bold text-4xl text-center py-2'>Gallery App</h1>
       <div className='flex h-[82%] flex-wrap gap-4 p-2'>
         {printUserData}
       </div>
@@ -53,9 +51,7 @@ const App = () => {
           className='bg-amber-400 text-sm cursor-pointer active:scale-95 text-black rounded px-4 py-2 font-semibold'
           onClick={() => {
             setUserData([])
-
             setIndex(index + 1)
-
           }}
         >
           Next
